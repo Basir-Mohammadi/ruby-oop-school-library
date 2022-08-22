a4d7efa6182349d553d7296d9f8e4369c404fa8d
unknown <sntakirutimana72@gmail.com>
2022-08-22 18:38:35 +0200

    Fix linter errors

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 require './student'
 
 def main
-  student_x = Student.new(29, 'Jaguar', '4th Grade')
+  student_x = Student.new(age: 29, name: 'Jaguar', classroom: '4th Grade')
   puts "Can student - <#{student_x.name}> use service?"
   puts "R/ #{student_x.can_use_service?}"
   puts student_x.play_hooky
