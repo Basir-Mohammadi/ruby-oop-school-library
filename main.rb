a516062b2f7eb69f2d71aec0b69b268587ee7408
unknown <sntakirutimana72@gmail.com>
2022-08-22 18:54:59 +0200

    Implement after-review required changes

--- main.rb
+++ main.rb
@@ -1,8 +1,8 @@
 require './student'
 
 def main
-  student_x = Student.new(age: 29, name: 'Jaguar', classroom: '4th Grade')
-  puts "Can student - <#{student_x.name}> use service?"
+  student_x = Student.new(age: 9, name: 'JD', classroom: '4th Grade')
+  puts "Can student named #{student_x.name} use service?"
   puts "R/ #{student_x.can_use_service?}"
   puts student_x.play_hooky
 end
