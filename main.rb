32f158a9305ed0fef7c5bd57c7087d9186ebdbbc
unknown <sntakirutimana72@gmail.com>
2022-08-23 12:33:18 +0200

    Create decorators and refactor one sole base class

--- main.rb
+++ main.rb
@@ -1,10 +1,19 @@
+require './person'
 require './student'
+require './decorators'
 
 def main
   student_x = Student.new(age: 9, name: 'JD', classroom: '4th Grade')
   puts "Can student named #{student_x.name} use service?"
   puts "R/ #{student_x.can_use_service?}"
   puts student_x.play_hooky
+
+  person = Person.new(age: 22, name: 'maximilianus')
+  puts "Original name is: #{person.correct_name}"
+  titled_person = CapitalizeDecorator.new(person)
+  puts "After capitalization, name is: #{titled_person.correct_name}"
+  titled_trimmed_person = TrimmerDecorator.new(titled_person)
+  puts "After both capitalization & trim, name is: #{titled_trimmed_person.correct_name}"
 end
 
 main
