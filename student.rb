7d36479d9ddffe6af9c4133fdf90fc66806b8e2f
unknown <sntakirutimana72@gmail.com>
2022-08-24 14:01:55 +0200

    Refactor both Student & Person classes to comply with new implementation

--- student.rb
+++ student.rb
@@ -1,9 +1,17 @@
 require './person'
 
 class Student < Person
+  attr_reader :classroom
+
   def initialize(age:, name:, classroom:, parent_permission: true)
     super(age: age, name: name, parent_permission: parent_permission)
-    @classroom = classroom
+
+    self.classroom = (classroom)
+  end
+
+  def classroom=(new_classroom)
+    @classroom = new_classroom
+    new_classroom.students << self unless new_classroom.students.include?(self)
   end
 
   def play_hooky
