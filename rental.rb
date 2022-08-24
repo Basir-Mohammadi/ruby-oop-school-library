4e2a09f17fb7ff9af73e83fbc427871294935596
unknown <sntakirutimana72@gmail.com>
2022-08-24 14:02:29 +0200

    Create new classes to count for associations

---
+++ rental.rb
@@ -0,0 +1,20 @@
+require 'time'
+require './book'
+require './person'
+
+class Rental
+  attr_reader :person, :book
+  attr_accessor :date
+
+  # @param person [Person]
+  # @param book [Book]
+  def initialize(person:, book:)
+    @date = DateTime.now.to_s
+
+    @person = person
+    person.rentals << self
+
+    @book = book
+    book.rentals << self
+  end
+end
